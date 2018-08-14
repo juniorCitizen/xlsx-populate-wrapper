3aa82239dc80344fc5562357e556e981cf7b8b1b
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-08-14 14:27:43 +0800

    basic functionality

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-module.exports = () => {
-  console.log('test')
-  return false
+const XlsxPopulate = require("xlsx-populate")
+
+class Xlsx {
+  constructor() {
+    this.filePath = null
+    this.workbook = null
+    this.worksheets = null
+  }
+
+  initialize(filePath) {
+    this.filePath = filePath
+    XlsxPopulate.fromFileAsync(filePath)
+      .then(workbook => {
+        this.workbook = workbook
+        this.worksheets = workbook.sheets()
+      })
+      .catch(error => Promise.reject(error))
+  }
+
+  get workbook() {
+    return this.workbook
+  }
+
+  get worksheets() {
+    return this.worksheets
+  }
 }
+
+module.exports = Xlsx
